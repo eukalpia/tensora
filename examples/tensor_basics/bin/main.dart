bc27b20c9688ab2f0e0d9624bc0241b4d48465c8
eukalpia <120781817+eukalpia@users.noreply.github.com>
2026-08-12 23:31:45 +0500

    style: format tensor basics example

--- examples/tensor_basics/bin/main.dart
+++ examples/tensor_basics/bin/main.dart
@@ -1,25 +1,9 @@
 import 'package:tensora/tensora.dart';
 
 void main() {
-  final a = Tensor.fromList(
-    [
-      1.0,
-      2.0,
-      3.0,
-      4.0,
-    ],
-    shape: Shape([2, 2]),
-  );
+  final a = Tensor.fromList([1.0, 2.0, 3.0, 4.0], shape: Shape([2, 2]));
 
-  final b = Tensor.fromList(
-    [
-      5.0,
-      6.0,
-      7.0,
-      8.0,
-    ],
-    shape: Shape([2, 2]),
-  );
+  final b = Tensor.fromList([5.0, 6.0, 7.0, 8.0], shape: Shape([2, 2]));
 
   final result = a.matmul(b);
 
