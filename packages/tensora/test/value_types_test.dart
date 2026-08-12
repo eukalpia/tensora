cd36f419ae6af6cc399b0c53168d34076c0d4bb5
eukalpia <120781817+eukalpia@users.noreply.github.com>
2026-08-13 00:12:08 +0500

    Define CUDA device value contract

--- packages/tensora/test/value_types_test.dart
+++ packages/tensora/test/value_types_test.dart
@@ -2,13 +2,35 @@ import 'package:tensora/tensora.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Milestone 1 exposes only the implemented dtype', () {
+  test('Tensora exposes the implemented dtype', () {
     expect(DType.values, [DType.float32]);
     expect(DType.float32.toString(), 'DType.float32');
   });
 
-  test('Milestone 1 exposes the CPU device', () {
+  test('CPU device remains stable', () {
     expect(Device.cpu.name, 'cpu');
+    expect(Device.cpu.index, 0);
+    expect(Device.cpu.isCpu, isTrue);
+    expect(Device.cpu.isCuda, isFalse);
     expect(Device.cpu.toString(), 'Device.cpu');
+  });
+
+  test('CUDA devices carry a validated index and value semantics', () {
+    final cuda0 = Device.cuda(0);
+    final anotherCuda0 = Device.cuda(0);
+    final cuda1 = Device.cuda(1);
+
+    expect(cuda0.name, 'cuda:0');
+    expect(cuda0.index, 0);
+    expect(cuda0.isCpu, isFalse);
+    expect(cuda0.isCuda, isTrue);
+    expect(cuda0, anotherCuda0);
+    expect(cuda0.hashCode, anotherCuda0.hashCode);
+    expect(cuda0, isNot(cuda1));
+    expect(cuda0.toString(), 'Device.cuda(0)');
+  });
+
+  test('CUDA device index cannot be negative', () {
+    expect(() => Device.cuda(-1), throwsArgumentError);
   });
 }
