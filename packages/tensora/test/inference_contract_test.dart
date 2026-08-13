cd385740d8c0678c7fba3942df40c3b9ebaae2bd
eukalpia <120781817+eukalpia@users.noreply.github.com>
2026-08-13 10:38:46 +0500

    Test ONNX execution provider contracts

--- packages/tensora/test/inference_contract_test.dart
+++ packages/tensora/test/inference_contract_test.dart
@@ -8,6 +8,25 @@ void main() {
     expect(OnnxRuntime.liveSessionCount, 0);
   });
 
+  test('ONNX provider values have stable runtime names', () {
+    expect(OnnxExecutionProvider.auto.runtimeName, 'auto');
+    expect(OnnxExecutionProvider.cpu.runtimeName, 'CPUExecutionProvider');
+    expect(OnnxExecutionProvider.cuda.runtimeName, 'CUDAExecutionProvider');
+    expect(
+      OnnxExecutionProvider.directML.runtimeName,
+      'DmlExecutionProvider',
+    );
+    expect(OnnxExecutionProvider.coreML.runtimeName, 'CoreMLExecutionProvider');
+    expect(
+      OnnxExecutionProvider.openVino.runtimeName,
+      'OpenVINOExecutionProvider',
+    );
+    expect(
+      OnnxExecutionProvider.miGraphX.runtimeName,
+      'MIGraphXExecutionProvider',
+    );
+  });
+
   test('empty ONNX model path is rejected before native work', () {
     expect(() => OnnxSession(''), throwsArgumentError);
     expect(() => OnnxSession('   '), throwsArgumentError);
@@ -16,6 +35,13 @@ void main() {
   test('session creation fails explicitly when ONNX backend is disabled', () {
     expect(
       () => OnnxSession('/tmp/model.onnx'),
+      throwsA(isA<UnsupportedOperationException>()),
+    );
+    expect(
+      () => OnnxSession(
+        '/tmp/model.onnx',
+        provider: OnnxExecutionProvider.cuda,
+      ),
       throwsA(isA<UnsupportedOperationException>()),
     );
     expect(OnnxRuntime.liveSessionCount, 0);
