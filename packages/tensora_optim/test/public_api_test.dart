cfd0fde24fb34ea00598810936c72cdbad00b295
eukalpia <120781817+eukalpia@users.noreply.github.com>
2026-08-13 18:17:31 +0500

    Test optimizer package namespace

---
+++ packages/tensora_optim/test/public_api_test.dart
@@ -0,0 +1,9 @@
+import 'package:tensora_optim/tensora_optim.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('optimizer API types are exported', () {
+    final List<Optimizer> values = <Optimizer>[];
+    expect(values, isEmpty);
+  });
+}
