3ffde46087609f3e3e2c6bfc4872fc15cad6b878
eukalpia <120781817+eukalpia@users.noreply.github.com>
2026-08-15 23:28:39 +0500

    feat: export safe parameter optimizer bridge

--- packages/tensora/lib/tensora.dart
+++ packages/tensora/lib/tensora.dart
@@ -6,5 +6,6 @@ export 'src/errors/tensora_exception.dart';
 export 'src/inference/onnx.dart';
 export 'src/shape/shape.dart';
 export 'src/tensor/tensor.dart';
+export 'src/training/native_parameter_optimizer.dart';
 export 'src/training/parameter.dart';
 export 'src/training/training.dart';
