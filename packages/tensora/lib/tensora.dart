e7dad1cb3ed62c0f973e8f5692bc18d5360cfd37
eukalpia <120781817+eukalpia@users.noreply.github.com>
2026-08-16 12:09:56 +0500

    fix: export NN V2 tensor state facade

--- packages/tensora/lib/tensora.dart
+++ packages/tensora/lib/tensora.dart
@@ -7,5 +7,6 @@ export 'src/inference/onnx.dart';
 export 'src/shape/shape.dart';
 export 'src/tensor/tensor.dart';
 export 'src/training/native_parameter_optimizer.dart';
+export 'src/training/native_tensor_state.dart';
 export 'src/training/parameter.dart';
 export 'src/training/training.dart';
